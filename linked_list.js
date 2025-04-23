e928d83f05239e765bd5cffec87f03e995fbfc2c
Andrew Perotti <Perotti28@gmail.com>
2025-04-23 11:49:50 -0700

    Add skeleton structure for class
    This includes stating all the methods that we'll need to declare so we can not only create linked lists but have functionality to manipulate them.

---
+++ linked_list.js
@@ -0,0 +1,28 @@
+export default class LinkedList{
+    constructor(){
+    }
+    
+    append(value){}
+
+    prepend(value){}
+
+    size(){}
+
+    head(){}
+
+    tail(){}
+
+    at(index){}
+
+    pop(){}
+
+    contains(value){}
+
+    find(value){}
+
+    toString(){}
+
+    insertAt(value, index){}
+
+    removeAt(index){}
+}
