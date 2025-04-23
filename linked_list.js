b228258382dde2cdda91451d4b7ffba034840fc2
Andrew Perotti <Perotti28@gmail.com>
2025-04-23 12:43:22 -0700

    Remove constructor logic, change append
    I had previously started instances of LinkedList with a value property === null and a next property === null, I have concluded that this will not work and we instead need to start with an empty object and have checks for it inside the methods.
    With those changes in effect i have changed the append method to check if the object is empty and if it is then to add the properties of the Node instance into this LinkedList object instead, otherwise if there is a pre existing node to then add on this new node to the end of the linked list.

--- linked_list.js
+++ linked_list.js
@@ -1,19 +1,21 @@
 import NodeInstance from "./node_instance.js";
 
 export default class LinkedList {
-  constructor(value = null, next = null) {
-    this.value = value;
-    this.next = null;
-  }
+  constructor() {}
 
   //adds a new node containing value to the end of the list
   append(value) {
-    //iterate to end of linked list and add this value as a node
-    let tempNode = this;
-    while (obj.next !== null) {
-      tempNode = tempNode.next;
+    //i think we need a way to add a first node, aka check if nodes or empty list
+    if (Object.keys(this) === 0) {
+      this.concat(new NodeInstance(value));
+    } else {
+      //iterate to end of linked list and add this value as a node
+      let tempNode = this;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new NodeInstance(value);
     }
-    tempNode.next = new NodeInstance(value);
   }
 
   // adds a new node containing value to the start of the list
@@ -46,10 +48,10 @@ export default class LinkedList {
     // parentheses if there is a value property/obj!==null, value, close parentheses, arrow, repeat unless null then null
     let resultString = ``;
     let currentNode = this;
-    while (currentNode.next !== null) {
-      resultString.push(`${value} -> `);
-    }
-    resultString.push("null");
+    // while (currentNode.next !== null) {
+    //   resultString.concat(`${currentNode.value} -> `);
+    // }
+    resultString.concat("null");
   }
 
   //that inserts a new node with the provided value at the given index.
