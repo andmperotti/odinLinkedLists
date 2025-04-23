c929ce3c542d8d49fc30076261626728e1acfd99
Andrew Perotti <Perotti28@gmail.com>
2025-04-23 11:48:23 -0700

    Add eslint config, including prettier integration
    This is mainly for vscode and to help me write better looking code.

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import {defineConfig} from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([eslintPluginPrettierRecommended]);
