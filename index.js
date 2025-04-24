4e237ff536f8bce65fc66c238386dbacba396242
Andrew Perotti <Perotti28@gmail.com>
2025-04-24 13:09:09 -0700

    Add an instance of the LinkedList and call methods
    I used these to test the classes and believe it works as intended and am going to call this done.

--- index.js
+++ index.js
@@ -11,8 +11,47 @@ list.append("snake");
 list.append("turtle");
 
 console.log(list.toString()); //( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
-list.prepend("drew");
-console.log(list.toString()); //( drew ) -> ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
-console.log(list.size()); //7
-console.log(list.head()); // {value: 'drew', next:....}
-console.log(list.tail()); // {value: 'turtle', next: null}
+
+//my invocations:
+let newList = new LinkedList();
+//add 5 values
+newList.append("drew");
+newList.append("steven");
+newList.append("john");
+newList.append("colleen");
+newList.append("ursula");
+console.log(newList.toString()); //(drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> null
+
+//test each method every which way you can
+newList.pop();
+console.log(newList.toString()); //(drew) -> (steven) -> (john) -> (colleen) -> null
+newList.append("ursula");
+console.log(newList.toString()); //(drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> null
+
+newList.prepend("steve");
+console.log(newList.toString()); //(steve) -> (drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> null
+
+console.log(newList.size()); //6
+
+console.log(newList.getHead()); //steve
+console.log(newList.tail()); //ursula
+
+console.log(newList.at(1)); //drew
+
+console.log(newList.contains("john")); //true
+console.log(newList.find("john")); //3
+
+newList.insertAt("gene", 0);
+console.log(newList.toString()); //(gene) -> (steve) -> (drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> null
+
+newList.insertAt("john", 1);
+console.log(newList.toString()); //(gene) -> (john) -> (steve) -> (drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> null
+
+newList.insertAt("caroline", 8);
+console.log(newList.toString()); //(gene) -> (john) -> (steve) -> (drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> (caroline) -> null
+
+newList.removeAt(0);
+console.log(newList.toString()); //(john) -> (steve) -> (drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> (caroline) -> null
+
+newList.removeAt(7);
+console.log(newList.toString()); //(john) -> (steve) -> (drew) -> (steven) -> (john) -> (colleen) -> (ursula) -> null
